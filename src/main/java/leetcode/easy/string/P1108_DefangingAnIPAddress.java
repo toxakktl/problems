3d79118158006d23dfb362d53d2a4add704c78f7
Tokhtar Yelemessov <t.yelemessov@gmail.com>
2021-11-25 22:32:59 +0600

    defang IP address

--- src/main/java/leetcode/easy/string/P1108_DefangingAnIPAddress.java
+++ src/main/java/leetcode/easy/string/P1108_DefangingAnIPAddress.java
@@ -1,2 +1,16 @@
-package leetcode.easy.string;public class P1108_DefangingAnIPAddress {
+package leetcode.easy.string;
+
+public class P1108_DefangingAnIPAddress {
+
+    public String defangIPaddr(String address) {
+        StringBuilder res = new StringBuilder();
+        for (char c: address.toCharArray()){
+            if (c == '.'){
+                res.append("[.]");
+            } else {
+                res.append(c);
+            }
+        }
+        return res.toString();
+    }
 }
