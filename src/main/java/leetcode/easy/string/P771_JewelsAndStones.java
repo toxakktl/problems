c9fbd87034cc232e2ff4801e611ca8b51f5617c5
Tokhtar Yelemessov <t.yelemessov@gmail.com>
2021-11-25 22:23:49 +0600

    Jewels and stones

---
+++ src/main/java/leetcode/easy/string/P771_JewelsAndStones.java
@@ -0,0 +1,25 @@
+package leetcode.easy.string;
+
+public class P771_JewelsAndStones {
+
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        for (char c: stones.toCharArray()){
+            if (jewels.indexOf(c) != -1)
+                count++;
+        }
+        return count;
+    }
+
+    public int numJewelsInStones2(String jewels, String stones) {
+        int[] cnt = new int[128];
+        for (char c: stones.toCharArray()){
+            cnt[c]++;
+        }
+        int ans = 0;
+        for (char c: jewels.toCharArray()) {
+            ans += cnt[c];
+        }
+        return ans;
+    }
+}
