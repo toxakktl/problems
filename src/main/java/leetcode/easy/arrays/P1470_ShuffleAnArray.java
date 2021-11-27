fdbd3e99bdaf3b2e331c5a7618bbbf09b33d5358
Tokhtar Yelemessov <t.yelemessov@gmail.com>
2021-11-27 14:46:22 +0600

    shuffling an array

---
+++ src/main/java/leetcode/easy/arrays/P1470_ShuffleAnArray.java
@@ -0,0 +1,29 @@
+package leetcode.easy.arrays;
+
+public class P1470_ShuffleAnArray {
+
+    public int[] shuffle(int[] nums, int n) {
+        int[] res = new int[2*n];
+        int temp = nums[0];
+        int j = 0;
+        for (int i = 0 ; i < nums.length; i++) {
+            if (i % 2 == 0) {
+                res[i] = temp;
+                temp = nums[++j];
+            } else {
+                res[i] = nums[n++];
+            }
+        }
+        return res;
+    }
+
+    public int[] shuffle2(int[] nums, int n) {
+        int[] res = new int[nums.length];
+        int j = 0;
+        for (int i = 0; i < n; i++){
+            res[j++] = nums[i];
+            res[j++] = nums[n+i];
+        }
+        return res;
+    }
+}
