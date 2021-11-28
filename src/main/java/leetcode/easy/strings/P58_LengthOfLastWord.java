7803d6d2f0537de85fc99d29da1edec76840c1d9
Tokhtar Yelemessov <t.yelemessov@gmail.com>
2021-11-28 11:53:52 +0600

    length of last word

---
+++ src/main/java/leetcode/easy/strings/P58_LengthOfLastWord.java
@@ -0,0 +1,17 @@
+package leetcode.easy.strings;
+
+public class P58_LengthOfLastWord {
+
+    public int lengthOfLastWord(String s) {
+        int len = 0;
+        for (int i = s.length() - 1; i >= 0; i--){
+            if (s.charAt(i) != ' '){
+                len++;
+            } else {
+                if (len > 0)
+                    return len;
+            }
+        }
+        return len;
+    }
+}
