f160f09982b158ab2ba95a7f7b93c5f47803c55b
Tokhtar Yelemessov <t.yelemessov@gmail.com>
2021-11-29 22:56:36 +0600

    valid palindrome

---
+++ src/main/java/leetcode/easy/strings/P125_ValidPalindrome.java
@@ -0,0 +1,24 @@
+package leetcode.easy.strings;
+
+public class P125_ValidPalindrome {
+
+    public boolean isPalindrome(String s) {
+        if (s == null || s.length() == 0)
+            return true;
+
+        int l = 0, r = s.length() - 1;
+        while (l < r) {
+            while (l < r && !Character.isLetterOrDigit(s.charAt(l))){
+                l++;
+            }
+            while (l < r && !Character.isLetterOrDigit(s.charAt(r))) {
+                r--;
+            }
+            if (Character.toLowerCase(s.charAt(l++)) != Character.toLowerCase(s.charAt(r--))){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
