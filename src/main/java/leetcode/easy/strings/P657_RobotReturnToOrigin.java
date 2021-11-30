28277529f02e7c4d69f14ed4965e328fee1040ed
Tokhtar Yelemessov <t.yelemessov@gmail.com>
2021-11-30 21:32:56 +0600

    robot return to origin

---
+++ src/main/java/leetcode/easy/strings/P657_RobotReturnToOrigin.java
@@ -0,0 +1,15 @@
+package leetcode.easy.strings;
+
+public class P657_RobotReturnToOrigin {
+    public boolean judgeCircle(String moves) {
+        int x = 0;
+        int y = 0;
+        for (char c: moves.toCharArray()) {
+            if (c == 'R') x++;
+            else if (c == 'L') x--;
+            else if (c == 'U') y++;
+            else y--;
+        }
+        return x == 0 && y == 0;
+    }
+}
