65f8051e54f30d9755289c002dde9ed534a638b7
Tokhtar Yelemessov <t.yelemessov@gmail.com>
2021-12-01 11:35:34 +0600

    palindromic substrings - dp and extended versions

--- src/main/java/leetcode/strings/medium/P647_PalindromicSubstrings.java
+++ src/main/java/leetcode/strings/medium/P647_PalindromicSubstrings.java
@@ -3,23 +3,46 @@ package leetcode.strings.medium;
 public class P647_PalindromicSubstrings {
 
     public static int countSubstrings(String str) {
-        if(str == null || str.length() < 1) return 0;
         int count = 0;
-        for(int i=0;i<str.length();i++){
-            count += countPalindromes(str, i, i); //Count even sized
-            count += countPalindromes(str, i, i+1); //Count odd sized
+        for (int i = 0; i < str.length(); i++) {
+            count += isPalindrome(str, i, i);
+            count += isPalindrome(str, i, i+1);
         }
         return count;
     }
 
-    private static int countPalindromes(String str, int s, int e){
+    private static int isPalindrome(String str, int start, int end) {
         int count = 0;
-        while(s>=0 && e<str.length() && str.charAt(s) == str.charAt(e)){
-            s--;
-            e++;
+        while (start >= 0 && end <= str.length()-1 && str.charAt(start--) == str.charAt(end++))
             count++;
-        }
         return count;
+    }
+
+    public static int countSubstringsDp(String str) {
+        if (str == null || str.length() == 0)
+            return 0;
+        int len = str.length();
+        boolean[][] dp = new boolean[len][len];
+        int res = 0;
+
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+            res++;
+        }
+        for (int i = 0; i < len-1; i++) {
+            dp[i][i+1] = str.charAt(i) == str.charAt(i+1);
+            if (dp[i][i+1])
+                res++;
+        }
+
+        for (int n = 3; n <= len; n++) {
+            for (int i = 0, j = i + len -1; j < n; i++, j++) {
+                dp[i][j] = str.charAt(i) == str.charAt(j) && dp[i+1][j-1];
+                if (dp[i][j])
+                    res++;
+            }
+        }
+        return res;
     }
 
     public static void main(String[] args) {
