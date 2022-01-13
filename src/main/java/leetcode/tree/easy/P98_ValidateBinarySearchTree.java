f2683fb0b09f6e2e5d41327380b0abd477bdac9b
Tokhtar Yelemessov <t.yelemessov@gmail.com>
2022-01-13 23:12:05 +0600

    validate bst

---
+++ src/main/java/leetcode/tree/easy/P98_ValidateBinarySearchTree.java
@@ -0,0 +1,17 @@
+package leetcode.tree.easy;
+
+import common.TreeNode;
+
+public class P98_ValidateBinarySearchTree {
+
+    public boolean isValidBST(TreeNode root) {
+        return  isValid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean isValid(TreeNode node, long min, long max) {
+        if (node == null) return true;
+        if (node.val <= min || node.val >= max) return false;
+        return isValid(node.left, min, node.val) && isValid(node.right, node.val, max);
+    }
+
+}
