4846f26493a083f3515c72055c6bccc21d2d2ba3
Tokhtar Yelemessov <t.yelemessov@gmail.com>
2022-01-13 22:50:49 +0600

    second version

--- src/main/java/leetcode/tree/easy/P94_BinaryTreeInorderTraversal.java
+++ src/main/java/leetcode/tree/easy/P94_BinaryTreeInorderTraversal.java
@@ -15,4 +15,22 @@ public class P94_BinaryTreeInorderTraversal {
         res.addAll(inorderTraversal(root.right));
         return res;
     }
+
+    private List<Integer> inorderTraversal2(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root, res);
+        return res;
+    }
+
+    private void helper(TreeNode root, List<Integer> res) {
+        if (root != null) {
+            if (root.left != null) {
+                helper(root.left, res);
+            }
+            res.add(root.val);
+            if (root.right != null) {
+                helper(root.right, res);
+            }
+        }
+    }
 }
