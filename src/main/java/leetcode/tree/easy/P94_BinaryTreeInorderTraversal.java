d06b00e33d87484459ada96416966c2cc1d8e724
Tokhtar Yelemessov <t.yelemessov@gmail.com>
2022-01-13 22:48:15 +0600

    inorder traversal

--- src/main/java/leetcode/tree/easy/P94_BinaryTreeInorderTraversal.java
+++ src/main/java/leetcode/tree/easy/P94_BinaryTreeInorderTraversal.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class P94_BinaryTreeInorderTraversal {
+
     private List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         if (root == null) return res;
