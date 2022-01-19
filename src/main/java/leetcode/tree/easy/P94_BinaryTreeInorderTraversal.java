20650d3b801ebf4ae7f27406db2f99c57232f872
Tokhtar Yelemessov <t.yelemessov@gmail.com>
2022-01-19 17:25:13 +0600

    stack based tree traversal of inorder

--- src/main/java/leetcode/tree/easy/P94_BinaryTreeInorderTraversal.java
+++ src/main/java/leetcode/tree/easy/P94_BinaryTreeInorderTraversal.java
@@ -4,6 +4,7 @@ import common.TreeNode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class P94_BinaryTreeInorderTraversal {
 
@@ -32,5 +33,20 @@ public class P94_BinaryTreeInorderTraversal {
                 helper(root.right, res);
             }
         }
+    }
+
+    private List<Integer> inorder3(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            res.add(root.val);
+            root = root.right;
+        }
+        return res;
     }
 }
