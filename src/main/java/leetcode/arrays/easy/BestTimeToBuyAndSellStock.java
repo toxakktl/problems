e8fb10345a60d23325e88823f83461cbbf8d3353
Tokhtar Yelemessov <t.yelemessov@gmail.com>
2022-03-02 00:21:13 +0600

    best time to buy and sell stock

--- src/main/java/leetcode/arrays/easy/BestTimeToBuyAndSellStock.java
+++ src/main/java/leetcode/arrays/easy/BestTimeToBuyAndSellStock.java
@@ -3,16 +3,13 @@ package leetcode.arrays.easy;
 public class BestTimeToBuyAndSellStock {
 
     public int maxProfit(int[] prices) {
-        if (prices == null || prices.length == 0)
-            return 0;
+        if (prices == null || prices.length == 0) return -1;
         int maxProfit = 0;
         int min = Integer.MAX_VALUE;
         for (int i = 0; i < prices.length; i++) {
-            if (prices[i] < min){
-                min = prices[i];
-            } else {
-                maxProfit = Math.max(maxProfit, prices[i] -min);
-            }
+            min = Math.min(min, prices[i]);
+            if (prices[i] > min)
+                maxProfit = Math.max(maxProfit, prices[i] - min);
         }
         return maxProfit;
     }
