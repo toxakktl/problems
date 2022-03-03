6be956378b373357e56bc2de809c25b0535f6323
Tokhtar Yelemessov <t.yelemessov@gmail.com>
2022-03-04 00:13:55 +0600

    linked list cycle, fast and slow pointers

---
+++ src/main/java/leetcode/linkedList/easy/P141_LinkedListCycle.java
@@ -0,0 +1,34 @@
+package leetcode.linkedList.easy;
+
+import common.ListNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class P141_LinkedListCycle {
+
+    public boolean hasCycle1(ListNode head) {
+        Set<ListNode> visited = new HashSet<>();
+        ListNode node = head;
+        while (node != null) {
+            if (visited.contains(node)) {
+                return true;
+            }
+            visited.add(node);
+            node = node.next;
+        }
+        return false;
+    }
+
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow)
+                return true;
+        }
+        return false;
+    }
+}
