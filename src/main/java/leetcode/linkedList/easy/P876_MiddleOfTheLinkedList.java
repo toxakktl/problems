1f1160c0190a4debbe5bad5c1bcb26dcb04a3b62
Tokhtar Yelemessov <t.yelemessov@gmail.com>
2022-03-13 20:15:39 +0600

    middle of the linked list

---
+++ src/main/java/leetcode/linkedList/easy/P876_MiddleOfTheLinkedList.java
@@ -0,0 +1,17 @@
+package leetcode.linkedList.easy;
+
+import common.ListNode;
+
+public class P876_MiddleOfTheLinkedList {
+
+    public ListNode middleNode(ListNode head) {
+        if (head == null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
