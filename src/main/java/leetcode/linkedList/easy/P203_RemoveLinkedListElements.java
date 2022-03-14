95e016c1e1e13292923258d2128a3699c5ac15e3
Tokhtar Yelemessov <t.yelemessov@gmail.com>
2022-03-14 22:29:21 +0600

    remove elements from linked list

---
+++ src/main/java/leetcode/linkedList/easy/P203_RemoveLinkedListElements.java
@@ -0,0 +1,38 @@
+package leetcode.linkedList.easy;
+
+import common.ListNode;
+
+public class P203_RemoveLinkedListElements {
+
+    public static ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = dummy;
+        while (cur.next != null) {
+            if (cur.next.val == val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode n1 = new ListNode(2);
+        ListNode n2 = new ListNode(6);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+        ListNode n5 = new ListNode(5);
+        ListNode n6 = new ListNode(6);
+        head.next = n1;
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        n5.next = n6;
+
+        removeElements(head, 6);
+    }
+}
